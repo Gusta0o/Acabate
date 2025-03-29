34323713838bf60b4f6c94bfe55df2751f684c29
Gusta <gustavohenriquemaia4@gmail.com>
2025-03-29 16:37:32 -0300

    aavoocaadoo

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function addAbacate() {
     // Adiciona um novo abacate após 10 cliques
     if (count >= 10) {
         // Cria uma nova div para o novo abacate
-        /*let newDiv = document.createElement("div");
+        let newDiv = document.createElement("div");
         newDiv.classList.add("container"); // Adiciona a classe de estilo para exibir como flex*/
         
         let newIndex = (lastIndex + 1) % abacates.length;
@@ -36,7 +36,7 @@ function addAbacate() {
         newDiv.appendChild(newAbct);
 
         // Adiciona a nova div ao contêiner original (abacateCont)
-        /*document.body.appendChild(newDiv);*/
+        document.body.appendChild(newDiv);
 
         count = 0; // Reseta o contador para o próximo ciclo
     }
